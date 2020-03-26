4197e5734faf4494d5f27007c3f80da4dfb2319c
jackheizer <jackheizer@users.noreply.github.com>
2020-03-25 22:39:43 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,9 @@
 #include <USBSerial.h>
 #include <calculator.h>
 
-Calculator calc;
-DigitalOut led(LED2);
-Serial serial(USBTX, USBRX);
+const Calculator calc;
+const DigitalOut led(LED2);
+const Serial serial(USBTX, USBRX);
 
 void blink_once(unsigned int delay_ms)
 {
@@ -16,17 +16,13 @@ void blink_once(unsigned int delay_ms)
 
 int main() {
     while(1) {
-        serial.printf("Addition: ");
-        serial.printf(calc.add(25, 17));
+        serial.printf("Addition: %d\n",calc.add(25, 17));
         blink_once(200);
-        serial.printf("Subtraction: ");
-        serial.printf(calc.sub(10, 3));
+        serial.printf("Subtraction: %d\n",calc.sub(10, 3));
         blink_once(200);
-        serial.printf("Multiplication: ");
-        serial.printf(calc.mul(3, 3));
+        serial.printf("Multiplication: %d\n",calc.mul(3, 3));
         blink_once(200);
-        serial.printf("Division: ");
-        serial.printf(calc.div(100, 3));
+        serial.printf("Division: %d\n",calc.div(100, 3));
         blink_once(200);
     }
 }
