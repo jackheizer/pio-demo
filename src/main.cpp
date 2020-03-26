5b9ad897df448df638d30799e3fd102751b45d72
jackheizer <jackheizer@users.noreply.github.com>
2020-03-25 22:23:40 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,32 @@
+#include <mbed.h>
+#include <USBSerial.h>
+#include <calculator.h>
+
+Calculator calc;
+DigitalOut led(LED2);
+Serial serial(USBTX, USBRX);
+
+void blink_once(unsigned int delay_ms)
+{
+    led = 1;
+    ThisThread::sleep_for(delay_ms);
+    led = 0;
+    ThisThread::sleep_for(delay_ms);
+}
+
 int main() {
-    return 0;
+    while(1) {
+        serial.printf("Addition: ");
+        serial.printf(calc.add(25, 17));
+        blink_once(200);
+        serial.printf("Subtraction: ");
+        serial.printf(calc.sub(10, 3));
+        blink_once(200);
+        serial.printf("Multiplication: ");
+        serial.printf(calc.mul(3, 3));
+        blink_once(200);
+        serial.printf("Division: ");
+        serial.printf(calc.div(100, 3));
+        blink_once(200);
+    }
 }
