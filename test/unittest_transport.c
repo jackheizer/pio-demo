7306ed837ad279a2702e786602df8697db26cd3c
jackheizer <jackheizer@users.noreply.github.com>
2020-03-26 22:11:12 -0600

    Create unittest_transport.c

---
+++ test/unittest_transport.c
@@ -0,0 +1,14 @@
+#include <mbed.h<
+#include "unittest_transport.h"
+
+static Serial serial(USBTX, USBRX);
+
+void unittest_uart_begin() {}
+
+void unittest_uart_putchar(char c) {
+    serial.printf("%c", c);
+}
+
+void unittest_uart_flush() {}
+
+void unittest_uart_end() {}
